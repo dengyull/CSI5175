b23d5b35ece27558be4f380990b23488e41a671e
Shuihuoyihao <127280237+Shuihuoyihao@users.noreply.github.com>
2023-03-25 16:37:48 -0400

    Add files via upload

--- app/src/main/java/com/example/csi5175/OrderHistorySecondFragment.kt
+++ app/src/main/java/com/example/csi5175/OrderHistorySecondFragment.kt
@@ -6,15 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.example.csi5175.databinding.FragmentSecondBinding
+import com.example.csi5175.databinding.FragmentOrderhistorysecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class OrderHistorySecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentOrderhistorysecondBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +24,7 @@ class OrderHistorySecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentOrderhistorysecondBinding.inflate(inflater, container, false)
         return binding.root
 
     }
