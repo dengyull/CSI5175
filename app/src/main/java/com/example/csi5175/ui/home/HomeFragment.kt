2aac663e5af598c9e057b8602efead6869dfe7dd
dengyull <43188363+dengyull@users.noreply.github.com>
2023-03-28 16:52:02 -0400

    function implement

--- app/src/main/java/com/example/csi5175/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/csi5175/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.example.csi5175.ui.home
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +19,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.csi5175.R
 import com.example.csi5175.databinding.FragmentHomeBinding
+import com.example.csi5175.model.Product
+import com.example.csi5175.persistence.AppDatabase
+import com.example.csi5175.productAdapter
 
 class HomeFragment : Fragment() {
     private lateinit var popularRestaurantsList: RecyclerView
@@ -50,14 +58,27 @@ class HomeFragment : Fragment() {
             val searchTerm = searchBar.text.toString()
             // Perform the search
         }
+
+        searchBar.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                // Handle search action here
+                true
+            } else {
+                false
+            }
+        }
 // Find views
         val popularCard = root.findViewById<CardView>(R.id.popular_card)
-        val popularRestaurantImage = root.findViewById<ImageView>(R.id.popular_restaurant_image)
-        val popularRestaurantName = root.findViewById<TextView>(R.id.popular_restaurant_name)
+        val RecyclerViewpopular = root.findViewById<RecyclerView>(R.id.RecyclerView_popular)
         val featuredCard = root.findViewById<CardView>(R.id.featured_card)
         val featuredCuisinesImage = root.findViewById<ImageView>(R.id.featured_cuisines_image)
         val featuredCuisinesName = root.findViewById<TextView>(R.id.featured_cuisines_name)
 
+
+        RecyclerViewpopular.layoutManager = LinearLayoutManager(requireContext())
+        val myDataset:List<Product> =  listOf()//Todo: favourlist
+        val adapter = productAdapter(myDataset)
+        RecyclerViewpopular.adapter = adapter
 
         // Set click listeners
         popularCard.setOnClickListener {
@@ -74,6 +95,27 @@ class HomeFragment : Fragment() {
 
 
 
+    private fun startVoiceInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now")
+        resultLauncher.launch(intent)
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = result!![0]
+            binding.searchBar.setText(spokenText)
+            // todo: start search
+
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
