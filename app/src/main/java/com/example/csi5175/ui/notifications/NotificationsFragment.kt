d77ba6629e8b5072b88741fe84ce7d193077161d
dengyull <43188363+dengyull@users.noreply.github.com>
2023-04-08 21:52:13 -0400

    calories

--- app/src/main/java/com/example/csi5175/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/csi5175/ui/notifications/NotificationsFragment.kt
@@ -47,10 +47,6 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         val firstName_change = binding.firstNameChangeInputs
         val lastName_change = binding.lastNameChangeInputs
         val phone_change = binding.phoneChangeInputs
